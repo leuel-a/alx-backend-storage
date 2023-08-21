aa0024762541b01494f2e96a167bfcf6406175c8
nefelibatala1993 <leuela1993@gmail.com>
2023-08-21 20:34:52 +0300

    try to pass the check for the eleveth task

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,6 +4,7 @@ from typing import List
 from pymongo.collection import Collection
 
 
-def update_topics(mongo_collection: Collection, name: str, topics: List[str]):
+def update_topics(mongo_collection: Collection, name: str, topics: List[str]) \
+                                                                    -> None:
     """Changes all topics of a school document based on the name"""
     mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
