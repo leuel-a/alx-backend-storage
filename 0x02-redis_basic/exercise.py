8e1ba6083ad43ffeff7c038872cdb07f7540968e
nefelibatala1993 <leuela1993@gmail.com>
2023-08-21 22:54:56 +0300

    add type annotation for the store function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,10 +7,11 @@ from redis import Redis
 class Cache:
     """Simple cache using redis-server"""
     def __init__(self):
+        """Instantiates a new cache object"""
         self._redis = Redis()
         self._redis.flushdb()
 
-    def store(self, data):
+    def store(self, data: str | bytes | int | float) -> str:
         """Stores the data with a uniquely generated id using
         an id generator and returns the key where the data is
         stored at"""
