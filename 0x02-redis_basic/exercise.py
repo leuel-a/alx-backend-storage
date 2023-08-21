205b1a13f9af08b33167a8bad3ebf64e64cdadb2
nefelibatala1993 <leuela1993@gmail.com>
2023-08-21 22:50:40 +0300

    add the gitignore and the first task of the project

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Defines the Cache class"""
+from uuid import uuid4
+from redis import Redis
+
+
+class Cache:
+    """Simple cache using redis-server"""
+    def __init__(self):
+        self._redis = Redis()
+        self._redis.flushdb()
+
+    def store(self, data):
+        """Stores the data with a uniquely generated id using
+        an id generator and returns the key where the data is
+        stored at"""
+        key = str(uuid4())  # The key where the data will be stored at
+        self._redis.set(key, data)
+        return key
