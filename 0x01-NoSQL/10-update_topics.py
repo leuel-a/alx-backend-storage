e89d014e5df896397613b7ec8b1b5e10e4f73972
nefelibatala1993 <leuela1993@gmail.com>
2023-08-21 20:44:00 +0300

    remove the eleventh and the twelvth task

--- 0x01-NoSQL/10-update_topics.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Defines the update_topics function"""
-from typing import List
-from pymongo.collection import Collection
-
-
-def update_topics(mongo_collection, name, topics):
-    """Changes all topics of a school document based on the name"""
-    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
