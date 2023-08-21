502d7fc6d94b5dddab87f553a7665f19f5ca698a
nefelibatala1993 <leuela1993@gmail.com>
2023-08-21 20:37:17 +0300

    removed type annotation for the functions

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,7 +4,6 @@ from typing import List
 from pymongo.collection import Collection
 
 
-def update_topics(mongo_collection: Collection, name: str, topics: List[str]) \
-                                                                    -> None:
+def update_topics(mongo_collection, name, topics):
     """Changes all topics of a school document based on the name"""
     mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
